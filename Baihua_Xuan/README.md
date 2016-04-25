5dff22d58933f5d98b96311a77946bd14715e08e
baihua <baihua.xuan@vanderbilt.edu>
2016-04-24 22:28:53 -0500

    Parsed header file to be used when reading in data

---
+++ Baihua_Xuan/README.md
@@ -0,0 +1,4 @@
+TODOs:
+
+1. Parse the attribute name file to get a list of attributes to use when populating the table
+2. Choose relevant attributes for analysis
--- Baihua_Xuan/predict_salary.py
+++ Baihua_Xuan/predict_salary.py
@@ -5,16 +5,21 @@ import pandas
     
 class predict_salary:
     def __init__(self, data_src, header_src):
-        self.header = []
+        
+        if (header_src is None):
+            # First line is the header
+            self.table = pandas.read_csv(data_src)
+        else:
+            with open(header_src, 'r') as myfile:
+                data = myfile.read().replace('\n', '')
+                self.headers = data.split(', ')
+                print self.headers
+                self.table = pandas.read_csv(data_src, self.header)
 
-        with open(header_src, 'r') as myfile:
-            data = myfile.read().replace('\n', '')
-            self.header = data.split(', ')
-            print self.header
+        #print self.table
 
-        self.table = pandas.read_csv(data_src, self.header)
-
+    
 
 if __name__ == '__main__':
     print 'Executing script for predicting salary'
-    predictor = predict_salary()
+    predictor = predict_salary('trial.txt', None)
